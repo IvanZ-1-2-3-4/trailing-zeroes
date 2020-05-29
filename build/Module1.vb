aa32d967509b0927cadc10f6080d73d7dd69db8c
Ivan Zabrodin <pyrat32@gmail.com>
2020-05-29 13:35:51 -0500

    Add files via upload

---
+++ build/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim answer As Integer
+        Dim out As Integer
+        Dim i As Integer = 1
+        While (True)
+            Console.WriteLine("Input number: ")
+            answer = Console.ReadLine()
+            out = 0
+            i = 1
+            While ((answer / 5 ^ i) > 1)
+                out = out + (Math.Floor(answer / (5 ^ i)))
+                i = i + 1
+            End While
+
+            Console.WriteLine(out)
+        End While
+    End Sub
+
+End Module
